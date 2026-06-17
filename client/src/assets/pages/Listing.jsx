337d1ace1dd9e1461fb7d20d3b48961568fdda39
aryan <arjitbhoi02@gmail.com>
2026-06-17 11:33:37 +0530

    Explicitly import React to fix 'React is not defined' ReferenceError

--- client/src/assets/pages/Listing.jsx
+++ client/src/assets/pages/Listing.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { Swiper,  SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
